c1b328586be2979cf1c71dd22948875eae22b8c5
AndyBuchwald <andy.buchwald@tu-dresden.de>
2022-04-01 13:08:58 +0200

    feat: get nodes, edges and layout from backend

--- src/components/NetworkGraph.vue
+++ src/components/NetworkGraph.vue
@@ -2,44 +2,20 @@
   <div class="NetGraph">
     <nav class="navbar navbar-light bg-light">
       <input
-        v-model="inputNode"
-        placeholder="Knoten"
+        v-model="metric"
+        placeholder="Metricname"
       >
       <label
         id="node_export"
         class="btn btn-outline-primary btn-navbar"
         @click="nodeButtonClicked"
-      >neuer Knoten</label>
-      <input
-        v-model="inputEdge1"
-        placeholder="Start"
-      >
-      <input
-        v-model="inputEdge2"
-        placeholder="Ziel"
-      >
-      <label
-        id="edge_export"
-        class="btn btn-outline-primary btn-navbar"
-        @click="edgeButtonClicked"
-      >neue Verbindung</label>
-      <input
-        v-model="randomNode"
-        placeholder="Anzahl Knoten"
-      >
-      <input
-        v-model="randomEdge"
-        placeholder="Anzahl Verbindungen"
-      >
-      <label
-        id="random_btn"
-        class="btn btn-outline-primary btn-navbar"
-        @click="randomButtonClicked"
-      >Zufall!</label>
+      >Metrik hinzufügen</label>
     </nav>
     <VNetworkGraph
       :nodes="nodes"
       :edges="edges"
+      :layouts="layouts"
+      :configs="configs"
       style="height: 500px"
     />
   </div>
@@ -49,53 +25,81 @@
 
 // Endpoint erzeugt JSON mit zufälligen Werten
 // in Vue Anzahl nodes/edges
+
+import { reactive } from 'vue'
+import * as vNG from 'v-network-graph'
+import { Nodes, Edges, Layouts } from 'v-network-graph'
+
 export default {
   name: 'NetworkGraph',
   components: {},
   data: function () {
     return {
       nodes: {
-        node1: { name: 'Node 1' },
-        node2: { name: 'Node 2' },
-        node3: { name: 'Node 3' },
-        node4: { name: 'Node 4' }
+        node1: { name: 'Node 1', type: 'circle', color: 'gray', size: 100 },
+        node2: { name: 'Node 2', type: 'rect' },
+        node3: { name: 'Node 3', type: 'circle', size: 20 },
+        node4: { name: 'Node 4', type: 'rect' }
       },
       edges: {
         edge1to2: { source: 'node1', target: 'node2' },
         edge2to3: { source: 'node2', target: 'node3' },
         edge3to4: { source: 'node3', target: 'node4' }
       },
-      inputNode: '',
-      inputEdge1: '',
-      inputEdge2: '',
-      randomNode: '',
-      randomEdge: ''
+      layouts: {
+        nodes: {
+          node1: { x: 0, y: 0 },
+          node2: { x: 50, y: 50 },
+          node3: { x: 100, y: 0 },
+          node4: { x: 150, y: 50 }
+        }
+      },
+      configs: reactive(
+        vNG.defineConfigs({
+          node: {
+            dragable: true,
+            normal: {
+              type: node => node.type,
+              radius: 20,
+              // for type is "rect" -->
+              width: 32,
+              height: 32,
+              borderRadius: 4,
+              // <-- for type is "rect"
+              strokeWidth: 0,
+              strokeColor: '#000000',
+              strokeDasharray: '0',
+              color: '#4466cc'
+            }
+          },
+          edge: {
+            marker: {
+              target: {
+                type: 'arrow',
+                width: 4,
+                height: 4,
+                margin: -1,
+                units: 'strokeWidth',
+                color: null
+              }
+            }
+          }
+        })
+      ),
+      metric: ''
     }
   },
   methods: {
     nodeButtonClicked: function () {
-      this.nodes = {}
-      const newNodeKey = 'node' + this.inputNode
-      const newNodeValue = { name: 'Node ' + this.inputNode }
-      this.nodes[newNodeKey] = newNodeValue
-    },
-    edgeButtonClicked: function () {
-      this.edges = {}
-      const newEdgeKey = 'edge' + this.inputEdge1 + 'to' + this.inputEdge2
-      const newEdgeValue = { source: 'node' + this.inputEdge1, target: 'node' + this.inputEdge2 }
-      this.edges[newEdgeKey] = newEdgeValue
-    },
-    randomButtonClicked: function () {
-      const NodeCount = this.randomNode === '' ? 3 : this.randomNode
-      const EdgeCount = this.randomEdge === '' ? 3 : this.randomEdge
-      const url = 'http://localhost:8080/' + NodeCount + '*' + EdgeCount
-      fetch(url)
-        .then(response => response.json())
-        .then(data => this.readJson(data))
+      const url = 'http://localhost:8000/api/explorer/' + this.metric
+      fetch(url).then(response => response.json()).then(data => this.readJson(data))
     },
     readJson: function (data) {
       this.nodes = data.nodes
       this.edges = data.edges
+      for (const [key, value] of Object.entries(data.layout)) {
+        this.layouts.nodes[key] = value
+      }
     }
   }
 }
