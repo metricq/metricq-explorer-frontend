07c4fc03994f51b6c382eeff7e5cb40bd9b7b40b
AndyBuchwald <andy.buchwald@tu-dresden.de>
2022-05-10 14:07:07 +0200

    feat: finished frontend

--- src/components/NetworkGraph.vue
+++ src/components/NetworkGraph.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="NetGraph">
     <nav class="navbar navbar-light bg-light">
-      <input
-        v-model="metric"
-        placeholder="Metricname"
-      >
-      <label
-        id="node_export"
-        class="btn btn-outline-primary btn-navbar"
-        @click="nodeButtonClicked"
-      >Metrik hinzufügen</label>
+      <div>
+        <img
+          src="../../public/img/metricq-logo.png"
+          width="35"
+          height="35"
+          alt="MetricQ-Logo"
+          style="border-radius: .25rem;"
+        >
+        MetricQ-Explorer
+      </div>
+      <div>
+        <input
+          v-model="metric"
+          placeholder="Metricname"
+        >
+        <label
+          id="node_export"
+          class="btn btn-outline-primary btn-navbar"
+          @click="nodeButtonClicked"
+        >Metrik hinzufügen</label>
+      </div>
     </nav>
     <VNetworkGraph
       :nodes="nodes"
@@ -23,29 +35,16 @@
 
 <script>
 
-// Endpoint erzeugt JSON mit zufälligen Werten
-// in Vue Anzahl nodes/edges
-
 import { reactive } from 'vue'
 import * as vNG from 'v-network-graph'
-import { Nodes, Edges, Layouts } from 'v-network-graph'
 
 export default {
   name: 'NetworkGraph',
   components: {},
   data: function () {
     return {
-      nodes: {
-        node1: { name: 'Node 1', type: 'circle', color: 'gray', size: 100 },
-        node2: { name: 'Node 2', type: 'rect' },
-        node3: { name: 'Node 3', type: 'circle', size: 20 },
-        node4: { name: 'Node 4', type: 'rect' }
-      },
-      edges: {
-        edge1to2: { source: 'node1', target: 'node2' },
-        edge2to3: { source: 'node2', target: 'node3' },
-        edge3to4: { source: 'node3', target: 'node4' }
-      },
+      nodes: {},
+      edges: {},
       layouts: {
         nodes: {
           node1: { x: 0, y: 0 },
@@ -66,10 +65,10 @@ export default {
               height: 32,
               borderRadius: 4,
               // <-- for type is "rect"
-              strokeWidth: 0,
-              strokeColor: '#000000',
-              strokeDasharray: '0',
-              color: '#4466cc'
+              color: node => node.color
+            },
+            label: {
+              direction: node => node.direction
             }
           },
           edge: {
@@ -111,6 +110,10 @@ export default {
 VNetworkGraph {
   height: 500px;
   width: 500px;
+}
+
+.btn {
+  margin-left: 10px;
 }
 
 </style>
